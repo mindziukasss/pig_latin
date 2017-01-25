2cd88b6a453fdf7895ba82e9271885579b5a2ea3
mindziukasss <mindziukasss@gmail.com>
2017-01-25 11:31:04 +0200

    Added translate.html, styled it

--- piglatin/views.py
+++ piglatin/views.py
@@ -3,7 +3,21 @@ from django.shortcuts import render
 
 def home(request):
 		return render(request, 'home.html')
-
 def translate(request):
 
-	return HttpResponse("You translate page" + request.GET['originaltext'] )
+	original = request.GET['originaltext'].lower()
+
+	translation = ''
+	for word in original.split():
+		if word[0] in ['a','e','i','o','u']:
+
+			translation += word
+			translation += 'yay '
+		else:
+			translation += word[1:]
+			translation += word[0]
+
+			
+			translation += 'ay '
+
+	return render(request, 'translate.html', {'original':original, 'translation':translation})
